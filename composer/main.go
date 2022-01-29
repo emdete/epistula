4a4a570b0f8316c287daa036af620b7f09028a5c
M. Dietrich <mdt@emdete.de>
2022-01-29 20:57:08 +0100

    implement switches

--- composer/main.go
+++ composer/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"log"
 	"os"
-	"io"
+	"fmt"
 	"strings"
 	"os/exec"
 	"io/ioutil"
 	"bufio"
 	//
 	"github.com/sendgrid/go-gmime/gmime"
-	"github.com/proglottis/gpgme"
+	_ "github.com/proglottis/gpgme"
 )
 
 const (
@@ -24,26 +24,38 @@ func main() {
 	log.Printf("main %#v", os.Args)
 	// The Idea is as follows: the composeser
 	// - is called with all information in its arguments like --from, --reply, --to, --subject, --cc, --bcc, ...
-	from := "M. Dietrich <mdt@emdete.de>"
-	var envelope *gmime.Envelope
-	switch len(os.Args) {
-	case 1:
-		// we compose a new email
-		envelope = nil
-	case 2:
-		// we reply to the given email
-		envelope = parseFile(os.Args[1]) // TODO for now it excepts a name to reply to only
-		if strings.HasPrefix(envelope.Header("Content-Type"), "multipart/encrypted; ") {
+	var meta_from_name, meta_from, meta_to, meta_cc, meta_bcc, meta_subject, content_text string
+	for i:=1;i<len(os.Args);i++ {
+		if strings.HasPrefix(os.Args[i], "--") {
+			x := strings.Split(os.Args[i][2:], "=")
+			switch x[0] {
+			case "from":
+				meta_from = x[1]
+			case "to":
+				meta_to = x[1]
+			case "cc":
+				meta_cc = x[1]
+			case "bcc":
+				meta_bcc = x[1]
+			case "subject":
+				meta_subject = x[1]
+			case "reply":
+				//
+			case "text":
+				content_text = x[1]
+			default:
+				panic(fmt.Sprintf("wrong arg: %s", os.Args[i]))
+			}
+		} else {
+			panic(fmt.Sprintf("wrong arg: %s", os.Args[i]))
 		}
-	default:
-		panic("wrongs arguments")
 	}
 	// - decrypt reply email
 	// - composes an email via gmime
 	var buffer []byte
 	if message, err := gmime.Parse(
 		"Date: Fri, 28 Jan 2022 23:59:04 +0100" + CRLF +
-		"From: " + from + CRLF +
+		"From: " + meta_from + CRLF +
 		"MIME-Version: 1.0" + CRLF +
 		"Content-Type: text/plain; charset=utf-8" + CRLF +
 		"Content-Transfer-Encoding: quoted-printable" + CRLF +
@@ -52,25 +64,12 @@ func main() {
 		panic(err)
 	} else {
 		// fields: from sender reply-to to cc bcc
-		//message.AddAddress("From", "M. Dietrich", "mdt@emdete.de")
-		//message.SetHeader("Content-Type", "text/plain")
-		if envelope != nil {
-			message.ParseAndAppendAddresses("To", envelope.Header("From"))
-		} else {
-			message.ParseAndAppendAddresses("To", "") // TODO how to add an empty "To:" ?
-		}
-		for _,name := range []string{"To", "Cc", "Bcc", } {
-			if envelope != nil {
-				message.ParseAndAppendAddresses(name, envelope.Header(name)) // TODO remove myself
-			} else {
-				message.ParseAndAppendAddresses(name, " ") // TODO how to add an empty "...:" ?
-			}
-		}
-		if envelope != nil {
-			message.SetSubject(envelope.Subject())
-		} else {
-			message.SetSubject(" ")
-		}
+		message.AddAddress("From", meta_from_name, meta_from)
+		message.SetHeader("Content-Type", "text/plain")
+		message.ParseAndAppendAddresses("To", meta_to) // TODO how to add an empty "To:" ?
+		message.ParseAndAppendAddresses("Cc", meta_cc)
+		message.ParseAndAppendAddresses("Bcc", meta_bcc)
+		message.SetSubject(meta_subject)
 		// Content-ID
 		// Content-Transfer-Encoding: quoted-printable
 		// Date: Thu, 13 Dec 2018 14:19:38 +0000
@@ -84,35 +83,19 @@ func main() {
 		message.SetHeader("X-Epistula-State", "I am not done")
 		message.SetHeader("X-Epistula-Comment", "This is your MUA talking to you. Add attachments as headerfield like below. Dont destroy the mail structure, if the outcome cant be parsed you will thrown into your editor again to fix it. Change the State to not contain 'not'.")
 		message.SetHeader("X-Epistula-Attachment", "#sample entry#")
-		if envelope != nil {
-			text := ""
-			if err := envelope.Walk(func (part *gmime.Part) error {
-				if part.IsText() && part.ContentType() == "text/plain" {
-					text = part.Text()
-				}
-				return nil
-			}); err != nil {
-				panic(err)
+		content_text = strings.ReplaceAll(content_text, "\n", "\n> ")
+		if err := message.Walk(func (part *gmime.Part) error {
+			if part.IsText() && part.ContentType() == "text/plain" {
+				part.SetText(content_text)
 			}
-			text = strings.ReplaceAll(text, "\n", "\n> ")
-			if err := message.Walk(func (part *gmime.Part) error {
-				if part.IsText() && part.ContentType() == "text/plain" {
-					part.SetText(text)
-				}
-				return nil
-			}); err != nil {
-				panic(err)
-			}
-		}
-		if b, err := message.Export(); err != nil {
+			return nil
+		}); err != nil {
 			panic(err)
-		} else {
-			buffer = b
 		}
 	}
 	// - exports it to a temp file
 	var tempfilename string
-	if f, err := os.CreateTemp("", "example"); err != nil {
+	if f, err := os.CreateTemp("", "epistula-composer-"); err != nil {
 		panic(err)
 	} else {
 		defer os.Remove(f.Name()) // clean up
@@ -126,6 +109,7 @@ func main() {
 	}
 	defer os.Remove(tempfilename)
 	// - execs the editor and waits for its termination
+	//if true { return }
 	programname := "nvim"
 	if programname, err := exec.LookPath(programname); err == nil {
 		var procAttr os.ProcAttr
@@ -156,24 +140,6 @@ func main() {
 func parseFile(filename string) *gmime.Envelope {
 	if fh, err := os.Open(filename); err != nil {
 		panic(err)
-	} else {
-		defer fh.Close()
-		if data, err := ioutil.ReadAll(bufio.NewReader(fh)); err != nil {
-			panic(err)
-		} else {
-			if envelope, err := gmime.Parse(string(data)); err != nil {
-				panic(err)
-			} else {
-				return envelope
-			}
-		}
-	}
-	return nil
-}
-
-func decryptMessage(stream io.Reader) *gmime.Envelope {
-	if fh, err := gpgme.Decrypt(stream); err != nil {
-		log.Printf("decryptMessage error %v", err)
 	} else {
 		defer fh.Close()
 		if data, err := ioutil.ReadAll(bufio.NewReader(fh)); err != nil {
